5660ae7e45e12f3a37bac1d1b1285a20c7a63123
Alexander Yakovis <coolspammail@gmail.com>
2019-03-14 23:01:34 +0000

    clean

--- simple-crypt.py
+++ simple-crypt.py
@@ -1,5 +1,6 @@
 import base64
 
+
 def encode(key, string):
     encoded_chars = []
     for i in range(len(string)):
@@ -7,8 +8,9 @@ def encode(key, string):
         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
         encoded_chars.append(encoded_c)
     encoded_string = ''.join(encoded_chars)
-    return encoded_string    
-    
+    return encoded_string
+
+
 def decode(key, string):
     decoded_chars = []
     for i in range(len(string)):
@@ -18,46 +20,45 @@ def decode(key, string):
     decoded_string = ''.join(decoded_chars)
     return decoded_string
 
-    
 
-def encode64(key, string):
-    encoded_chars = []
-    for i in xrange(len(string)):
-        key_c = key[i % len(key)]
-        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
-        encoded_chars.append(encoded_c)
-    encoded_string = "".join(encoded_chars)
-    return base64.urlsafe_b64encode(encoded_string)
-    
-def decode64(key, string):
-    string=base64.urlsafe_b64decode(string)
-    decoded_chars = []
-    for i in xrange(len(string)):
-        key_c = key[i % len(key)]
-        decoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
-        decoded_chars.append(decoded_c)
-    decoded_string = "".join(decoded_chars)  
-    return (decoded_string)
+# def encode64(key, string):
+#     encoded_chars = []
+#     for i in range(len(string)):
+#         key_c = key[i % len(key)]
+#         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+#         encoded_chars.append(encoded_c)
+#     encoded_string = "".join(encoded_chars)
+#     return base64.urlsafe_b64encode(encoded_string)
+
+
+# def decode64(key, string):
+#     string = base64.urlsafe_b64decode(string)
+#     decoded_chars = []
+#     for i in range(len(string)):
+#         key_c = key[i % len(key)]
+#         decoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
+#         decoded_chars.append(decoded_c)
+#     decoded_string = "".join(decoded_chars)
+#     return (decoded_string)
 
 
 # Start
-message="Privet"
-key="enckey"
+message = "Privet"
+key = "enckey"
 
 # Simple
-print("original:")    
-print(message)    
-encoded=encode(key, message)
+print("original:")
+print(message)
+encoded = encode(key, message)
 print("encoded: " + ''.join([encoded]))
-print([encoded]);
-decoded=decode(key, encoded)
+print([encoded])
+decoded = decode(key, encoded)
 print("decoded: " + decoded)
 
 
-# 64 Encode
-print("\n\nUrlsafe")    
-encoded=encode64(key, message)
-print("encoded: " + encoded)
-decoded=decode64(key, encoded)
-print("decoded: " + decoded)
-
+## 64 Encode
+# print("\n\nUrlsafe")
+# encoded = encode64(key, message)
+# print("encoded: " + encoded)
+# decoded = decode64(key, encoded)
+# print("decoded: " + decoded)
